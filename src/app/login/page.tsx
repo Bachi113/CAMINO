592da563a7236e01b5e04c124b482ea22721f717
Hevin <hevinvnsgu@gmail.com>
2024-06-06 14:29:57 +0530

    wip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,21 +3,41 @@
 // The application name is dynamically fetched from the config and displayed at the top.
 // The `EmailAuth` and `GoogleAuth` components are used here for handling the respective authentication methods.
 
+import Logo from '@/components/Logo';
 import EmailAuth from '@/components/auth/EmailAuth';
 import GoogleAuth from '@/components/auth/GoogleAuth';
+import { Button } from '@/components/ui/button';
 import config from '@/config';
+import { MdOutlineStoreMallDirectory } from 'react-icons/md';
+import { FiUser } from 'react-icons/fi';
 
 export default function Login() {
   return (
-    <div className='h-screen flex'>
-      <div className='w-full flex flex-col items-center justify-center px-6'>
-        <h1 className='text-4xl font-medium mb-4'>{config.app.name}</h1>
-        <h2 className='text-[#8F95B2] text-sm mb-12'>Login or register with your email</h2>
-
-        <div className='w-full sm:max-w-md flex flex-col gap-8 items-center'>
-          <GoogleAuth />
-          <hr className='w-full bg-[#CDCDCD]' />
-          <EmailAuth />
+    <div className='h-screen flex flex-col'>
+      <div className='w-full flex flex-col items-center mt-36'>
+        <div className='m-4 md:m-0 md:max-w-[414px]'>
+          <div className='border rounded-lg p-8 space-y-10'>
+            <div className='space-y-6 flex flex-col items-center justify-center'>
+              <Logo />
+              <p className='text-2xl font-semibold leading-7 text-default'>Welcome to SaveX</p>
+            </div>
+            <div className='space-y-4'>
+              <Button
+                size='lg'
+                variant='secondary'
+                className='w-full text-default text-sm font-semibold leading-5 gap-2'>
+                <MdOutlineStoreMallDirectory className='size-6' />
+                Merchat Login
+              </Button>
+              <Button
+                size='lg'
+                variant='secondary'
+                className='w-full text-default text-sm font-semibold leading-5 gap-2'>
+                <FiUser className='size-6' />
+                Personal Login
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,7 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        default: 'hsl(var(--default))',
         tremor: {
           brand: {
             faint: colors.blue[50],
